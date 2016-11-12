d9c20767b6e523b9638f301aa31cdd36377919f4
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-11-12 14:17:47 +0300

    Добавлены стили для расширенного модального окна

--- markup/components/advanced-popup/advanced-popup.js
+++ markup/components/advanced-popup/advanced-popup.js
@@ -1,1 +1,22 @@
+/*eslint-disable*/
+var imgsLeftTopPart = $('.advanced-popup .left-part-top-part .slider-photo img');
+var heightsLeftTopPars = [];
+var widthLeftTopPars = [];
+$.each(imgsLeftTopPart, function (i, el) {
+    heightsLeftTopPars.push(el.height);
+    widthLeftTopPars.push(el.width);
+});
+var maxHeightImgLeftTopPart = Math.max(...heightsLeftTopPars);
+var maxWidthImgLeftTopPart = Math.max(...widthLeftTopPars);
+$('.left-part-top-part .slider-photo').height(maxHeightImgLeftTopPart).width(maxWidthImgLeftTopPart);
+$('.right-part-top-part .slider-photo').height(maxHeightImgLeftTopPart).width(maxWidthImgLeftTopPart);
 
+var itemsCommentText = $('.advanced-popup .left-part-advanced-popup .bottom-part .slider-photo li');
+var heightItemsCommentText = [];
+$.each(itemsCommentText, function (i, el) {
+    console.log(el.height);
+});
+// var maxHeightItemsCommentText = Math.max(...heightItemsCommentText);
+// console.log(maxHeightItemsCommentText);
+// $('.advanced-popup .left-part-advanced-popup .bottom-part').height(maxHeightItemsCommentText);
+/*eslint-enable*/
